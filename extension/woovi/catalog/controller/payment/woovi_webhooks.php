dc4a4ca0f2fe236b35dfe1c30d4703c71f944bc1
Eduardo Maciel <54045014+edumaciel10@users.noreply.github.com>
2023-07-11 17:35:23 -0300

    fix(webhook): fix test call and receveid webhook (#45)
    * fix(webhook): fix test call and receveid webhook
    
    * fix(webhook): more easily to read

--- extension/woovi/catalog/controller/payment/woovi_webhooks.php
+++ extension/woovi/catalog/controller/payment/woovi_webhooks.php
@@ -6,7 +6,7 @@ use Opencart\System\Engine\Controller;
 
 /**
  * Invoked by the Woovi API.
- * 
+ *
  * @property \OpenPix\PhpSdk\Client $woovi_api_client
  * @property \Woovi\Opencart\Logger $woovi_logger
  */
@@ -24,24 +24,66 @@ class WooviWebhooks extends Controller
         $this->load->model("checkout/order");
 
         $payload = $this->getValidatedPayload();
-
         if (is_null($payload)) return;
 
-        $this->handleTransactionReceivedWebhook($payload);
+        $this->handleWebhookEvents($payload);
     }
 
+    public function isTestPayload($payload) : bool {
+        if(isset($payload['evento']) && $payload['evento'] === 'teste_webhook') {
+            return true;
+        }
+
+        if(isset($payload['event']) && $payload['event'] === 'teste_webhook') {
+            return true;
+        }
+
+        return false;
+    }
+
+    public function handleWebhookEvents($payload): void {
+
+        if($this->isTestPayload($payload)) {
+            $this->emitJson(["message" => "Success."]);
+            $this->woovi_logger->debug("Test Webhook received.", "catalog/webhooks");
+            return;
+        }
+
+
+        if(!isset($payload['event']) || empty($payload['event'])) {
+            $this->emitJson(["error" => "Invalid webhook payload."], 400);
+            return;
+        }
+
+        $event = $payload['event'];
+
+        // @todo: implements this
+        if($event === 'opencart-configure') {
+            $this->emitJson(["message" => "Success."]);
+            $this->woovi_logger->debug("Configure Webhook configure.", "catalog/webhooks");
+            return;
+        }
+
+        if (
+            $event === 'OPENPIX:TRANSACTION_RECEIVED' ||
+            $event === 'OPENPIX:CHARGE_COMPLETED'
+        ) {
+            $this->handleTransactionReceivedWebhook($payload);
+            return;
+        }
+
+
+    }
     /**
      * Executed when an PIX transaction is received.
      */
     private function handleTransactionReceivedWebhook(array $payload): void
     {
-        $payload["charge"]["correlationID"] = "not-found-correlation-id";
-
         $order = $this->model_extension_woovi_payment_woovi_order->getOpencartOrderByCorrelationID($payload["charge"]["correlationID"]);
 
         if (empty($order)) {
             $this->woovi_logger->notice("Cound not find OpenCart order with correlation ID `" . $payload["charge"]["correlationID"] . "`", "catalog/webhooks");
-            $this->emitJson(["error" => "Order not found."], 404);
+            $this->emitJson(["error" => "Order not found."], 400);
             return;
         }
 
@@ -59,15 +101,39 @@ class WooviWebhooks extends Controller
         $this->emitJson(["message" => "Success."]);
     }
 
+    private function validSignature($payload, $signature): bool
+    {
+        if (empty($signature) || ! $this->woovi_api_client->webhooks()->isWebhookValid($payload, $signature)) {
+            return false;
+        }
+
+        return true;
+    }
+
+    public function isValidWebhookPayload($data)
+    {
+        if (!isset($data['event']) || empty($data['event'])) {
+            if (!isset($data['evento']) || empty($data['evento'])) {
+                return false;
+            }
+        }
+
+        // @todo remove it and update evento to event
+
+        return true;
+    }
+
     /**
      * Validates the webhook request and returns validated data or `null` if an error occurs.
      */
     private function getValidatedPayload(): ?array
-    {        
+    {
         $rawPayload = file_get_contents("php://input");
-        $signature = getallheaders()["x-webhook-signature"] ?? null;
-       
-        if (empty($signature) || ! $this->woovi_api_client->webhooks()->isWebhookValid($rawPayload, $signature)) {
+        $headers = getallheaders();
+
+        $signature = $headers["X-Webhook-Signature"];
+
+        if(! $this->validSignature($rawPayload, $signature)) {
             $serializedRequest = json_encode($this->request, JSON_PRETTY_PRINT);
 
             $this->woovi_logger->warning(
@@ -80,11 +146,14 @@ class WooviWebhooks extends Controller
         }
 
         $payload = json_decode($rawPayload, true);
+
+
         $isJsonInvalid = json_last_error() !== JSON_ERROR_NONE;
 
-        if ($isJsonInvalid
-            || ! is_array($payload)
-            || ! $this->isWebhookPayloadValid($payload)) {
+        if (
+            $isJsonInvalid ||
+            !is_array($payload) ||
+            !$this->isWebhookPayloadValid($payload)) {
             $serializedRequest = json_encode($this->request, JSON_PRETTY_PRINT);
 
             $this->woovi_logger->warning(
@@ -105,16 +174,18 @@ class WooviWebhooks extends Controller
     }
 
     /**
-     * Checks if webhook payload is valid.
+     * Check if the provided data is a valid webhook payload.
+     *
+     * @param array $data The data to be validated.
+     *
+     * @return bool Returns true if the data contains any of the required keys (charge, pix, or event), otherwise returns false.
      */
     private function isWebhookPayloadValid(array $payload): bool
     {
-        if (! isset($payload["charge"]) || ! isset($payload["charge"]["correlationID"])) {
-            return false;
-        }
-
-        if (! isset($payload["pix"]) || ! isset($payload["pix"]["endToEndId"])) {
-            return false;
+        if (!isset($payload['event']) || empty($payload['event'])) {
+            if (!isset($payload['evento']) || empty($payload['evento'])) {
+                return false;
+            }
         }
 
         return true;

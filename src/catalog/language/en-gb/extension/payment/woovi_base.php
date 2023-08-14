ef60ea75e67e5891ec831e1f59b6d1a5d3e63c99
Cristiano <cris.moraes.ds@gmail.com>
2023-08-13 21:13:08 -0300

    fix(checkout): add language file

---
+++ src/catalog/language/en-gb/extension/payment/woovi_base.php
@@ -0,0 +1,18 @@
+<?php
+
+/** @var array<string, string> $_ */
+
+$_["Confirm Order"] = "Confirm Order";
+
+$_["No order ID in the session!"] = "No order ID in the session!";
+$_["Payment method is incorrect!"] = "Payment method is incorrect!";
+$_["There is already a charge for this order!"] = "There is already a charge for this order!";
+
+$_["CPF/CNPJ invalid! Change the CPF/CNPJ field in your account settings page or on this page if you see the field."] = "CPF/CNPJ invalid! Change the CPF/CNPJ field in your account settings page or on this page if you see the field.";
+$_["CPF/CNPJ invalid!"] = "CPF/CNPJ invalid!";
+
+$_["CPF/CNPJ"] = "CPF/CNPJ";
+
+$_["An error occurred while creating the Pix charge."] = "An error occurred while creating the Pix charge.";
+
+$_["Missing customer data on checkout."] = "Missing customer data on checkout.";

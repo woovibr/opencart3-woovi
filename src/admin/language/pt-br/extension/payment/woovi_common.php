8b03dff5225469dece9fbe28c57e6ab6c55f5a1e
criskell <96352451+criskell@users.noreply.github.com>
2023-08-19 23:37:41 -0300

    feat(i18n): add pt-br support for admin (#66)

---
+++ src/admin/language/pt-br/extension/payment/woovi_common.php
@@ -0,0 +1,46 @@
+<?php
+
+/** @var array<string, string> $_ */
+
+$_["Home"] = "Página inicial";
+$_["Extensions"] = "Extensões";
+
+$_["Save"] = "Salvar";
+$_["Previous page"] = "Página anterior";
+
+$_["Edit General Settings"] = "Editar configurações gerais";
+$_["Edit Pix Settings"] = "Edit configurações do Pix";
+$_["Edit Woovi Parcelado Settings"] = "Editar configurações do Woovi Parcelado";
+$_["Edit checkout field settings"] = "Editar configurações de campo do checkout";
+
+$_["Status"] = "Status";
+
+$_["App ID"] = "App ID";
+$_["Enter your Woovi App ID"] = "Digite seu App ID da Woovi";
+
+$_["Sort Order"] = "Ordem de classificação";
+
+$_["Payment method title"] = "Título da forma de pagamento";
+
+$_["Change order status after issuing the pix to:"] = "Altere o status do pedido após emitir o pix para:";
+$_["Change order status after pix charge is paid to:"] = "Altere o status do pedido após o pagamento da cobrança do pix para:";
+
+$_["Notify customer when order is paid?"] = "Notificar o cliente quando o pedido for pago";
+
+$_["Which custom field to use for CPF/CNPJ?"] = "Qual custom field usar para CPF/CNPJ?";
+$_["Create an custom field here."] = "Crie um custom field aqui.";
+$_["Which custom field to use for the customer address house number?"] = "Qual custom field usar para o número da casa do endereço do cliente?";
+$_["Which custom field to use for the customer address complement?"] = "Qual custom field usar para o complemento de endereço do cliente?";
+
+$_["Warning: You do not have permission to modify Pix settings!"] = "Aviso: você não tem permissão para modificar as configurações do Pix!";
+$_["Success: You have modified Pix settings!"] = "Sucesso: Você modificou as configurações do Pix!";
+
+$_["Use this Webhook URL to be registered at Woovi:"] = "Use esta URL do Webhook para se registrar no Woovi:";
+$_["Open your account now"] = "Abra sua conta agora";
+$_["Woovi integration <a href=\"%woovi_opencart_documentation_url%\" target=\"_blank\">documentation</a> with OpenCart"] = "<a href=\"%woovi_opencart_documentation_url%\" target=\"_blank\">Documentação</a> da integração Woovi com OpenCart";
+
+$_["None"] = "None";
+
+$_["CPF/CNPJ"] = "CPF/CNPJ";
+$_["Address number"] = "Número do endereço";
+$_["Address complement"] = "Complemento do endereço";

2d503ead036cbc1f96512c2406ecd0401e376726
criskell <96352451+criskell@users.noreply.github.com>
2023-08-19 23:23:06 -0300

    fix(webhooks): use upsert on app id configuration (#64)

--- src/catalog/model/extension/payment/woovi_webhooks.php
+++ src/catalog/model/extension/payment/woovi_webhooks.php
@@ -10,15 +10,36 @@ class ModelExtensionPaymentWooviWebhooks extends Model
     /**
      * Configure new App ID.
      */
-    public function configureAppId(string $newAppId): void
+    public function configureAppId(string $newAppId, int $storeId = 0): void
     {
+        $storeId = (int) $this->db->escape($storeId);
+        $newAppId = $this->db->escape($newAppId);
+        
+        $query = $this->db->query("
+            SELECT `value` FROM " . DB_PREFIX . "setting
+            WHERE store_id = '" . $storeId . "'
+                AND `key` = 'payment_woovi_app_id'
+        ");
+
+        if (! $query->num_rows) {
+            $this->db->query("
+                INSERT INTO `" . DB_PREFIX . "setting`
+                SET `store_id` = '" . $storeId . "',
+                    `code` = 'payment_woovi',
+                    `key` = 'payment_woovi_app_id',
+                    `value` = '" . $newAppId . "'
+            ");
+
+            return;
+        }
+
         $this->db->query("
             UPDATE `" . DB_PREFIX . "setting`
-            SET `value` = '" . $this->db->escape($newAppId)
+            SET `value` = '" . $newAppId
                 . "', `serialized` = '0'
             WHERE `code` = 'payment_woovi'
                 AND `key` = 'payment_woovi_app_id'
-                AND `store_id` = '0'
+                AND `store_id` = '" . $storeId . "'
         ");
     }
 }

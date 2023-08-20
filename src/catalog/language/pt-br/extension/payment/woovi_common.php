e81a333279ecf294766236f91783e2710ee60cd6
Cristiano <cris.moraes.ds@gmail.com>
2023-08-19 23:41:08 -0300

    feat(i18n): add pt-br support for catalog

---
+++ src/catalog/language/pt-br/extension/payment/woovi_common.php
@@ -0,0 +1,20 @@
+<?php
+
+/** @var array<string, string> $_ */
+
+$_["Confirm Order"] = "Confirmar pedido";
+
+$_["No order ID in the session!"] = "Nenhum ID do pedido na sessão!";
+$_["Payment method is incorrect!"] = "A forma de pagamento está incorreta!";
+$_["There is already a charge for this order!"] = "Já existe uma cobrança para este pedido!";
+
+$_["CPF/CNPJ invalid! Change the CPF/CNPJ field in your account settings page or on this page if you see the field."] = "CPF/CNPJ inválido! Altere o campo CPF/CNPJ na página de configurações da sua conta ou nesta página, caso veja o campo.";
+$_["CPF/CNPJ invalid!"] = "CPF/CNPJ inválido!";
+
+$_["CPF/CNPJ"] = "CPF/CNPJ";
+$_["Address number"] = "Número do endereço";
+$_["Address complement"] = "Complemento do endereço";
+
+$_["An error occurred while creating the Pix charge."] = "Ocorreu um erro ao criar a cobrança do Pix.";
+
+$_["Missing customer data on checkout."] = "Há falta de dados do cliente no checkout.";

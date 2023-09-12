32f9c176724857ce90ec6cc05dc333fb7db6bbf4
criskell <cris.moraes.ds@gmail.com>
2023-09-12 11:16:46 -0300

    chore(config): fix dev envs

--- src/system/config/woovi.development.php
+++ src/system/config/woovi.development.php
@@ -2,7 +2,7 @@
 
 $_["woovi_env"] = [
     "env" => "development",
-    "apiUrl" => "http://localhost:5001",
+    "apiUrl" => "http://host.docker.internal:5001",
     "pluginUrl" => "http://localhost:4444/woovi.js",
     "platformUrl" => "http://localhost:8103"
 ];
